4b3587af3bdadddad3899429f9492ca7bfa08022
jorgelucas <jorgelucasl91@gmail.com>
2021-09-09 11:27:45 -0300

    [TEST] Testes de OrganizationTypeAppService

---
+++ aspnet-core/test/ThinkAM.ThinkEvent.Tests/Organizations/OrganizationTestBogusFixture.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ThinkAM.ThinkEvent.Organizations.Dto;
+using Xunit;
+
+namespace ThinkAM.ThinkEvent.Tests.Organizations
+{
+
+    [CollectionDefinition(nameof(OrganizationsBogusCollection))]
+    public class OrganizationsBogusCollection : ICollectionFixture<OrganizationTestBogusFixture>
+    { }
+    public class OrganizationTestBogusFixture: ThinkEventBogustBase
+    {
+        public IEnumerable<CreateOrganizationTypeDto> GenerateCreateOrganizationTypeDto(int length)
+        {
+
+            var organizationTypes = GetFaker<CreateOrganizationTypeDto>()
+                .CustomInstantiator(f => new CreateOrganizationTypeDto
+                {
+                    Name = f.Name.JobType()
+                });
+            return organizationTypes.Generate(length);
+        }
+
+        public CreateOrganizationTypeDto GetValidCreateOrganizationTypeDto()
+        {
+            return GenerateCreateOrganizationTypeDto(1).FirstOrDefault();
+        }
+
+    }
+}
---
+++ aspnet-core/test/ThinkAM.ThinkEvent.Tests/Organizations/OrganizationTypeAppService_Tests.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using Abp.Application.Services.Dto;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+using ThinkAM.ThinkEvent.Organizations;
+using Xunit;
+
+namespace ThinkAM.ThinkEvent.Tests.Organizations
+{
+    [Collection(nameof(OrganizationsBogusCollection))]
+    public class OrganizationTypeAppService_Tests : ThinkEventTestBase
+    {
+        private readonly IOrganizationTypeAppService _organizationTypeAppService;
+        private readonly OrganizationTestBogusFixture _organizationTestFixture;
+
+        public OrganizationTypeAppService_Tests(OrganizationTestBogusFixture organizationTestFixture)
+        {
+            _organizationTestFixture = organizationTestFixture;
+            _organizationTypeAppService = Resolve<IOrganizationTypeAppService>(); ;
+        }
+
+        [Fact]
+        public async Task GetOrganizationTypes_Test()
+        {
+            // Arrange
+            var createOrganizationTypeDto = _organizationTestFixture.GetValidCreateOrganizationTypeDto();
+            await _organizationTypeAppService.CreateAsync(createOrganizationTypeDto);
+
+            // Act
+            var output = await _organizationTypeAppService.GetAllAsync(new PagedResultRequestDto { MaxResultCount = 1 });
+
+            // Assert
+            output.Items.Count.ShouldBeGreaterThan(0);
+        }
+
+        [Fact]
+        public async Task CreateOrganizationType_Test()
+        {
+            // Arrange
+            var createOrganizationTypeDto = _organizationTestFixture.GetValidCreateOrganizationTypeDto();
+
+            // Act
+            await _organizationTypeAppService.CreateAsync(createOrganizationTypeDto);
+
+            // Assert
+            await UsingDbContextAsync(async context =>
+            {
+                var organizationType = await context.OrganizationTypes.FirstOrDefaultAsync(u => u.Name == createOrganizationTypeDto.Name);
+                organizationType.ShouldNotBeNull();
+            });
+        }
+
+
+
+    }
+}
